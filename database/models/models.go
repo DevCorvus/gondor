73da016bf7e61b3f084eb8cb40a4ddbb9ce3715b
Luis Portillo <devcorvus.contact@gmail.com>
2022-10-06 22:04:53 -0400

    feat: adding name to User model

--- database/models/models.go
+++ database/models/models.go
@@ -8,8 +8,9 @@ import (
 
 type User struct {
 	ID        uint            `json:"id" gorm:"primarykey"`
-	Email     string          `json:"email" validate:"required,email"`
-	Password  string          `json:"password" validate:"required,min=6"`
+	Name      string          `json:"name" validate:"required,min=2,max=100"`
+	Email     string          `json:"email" validate:"required,email,max=250"`
+	Password  string          `json:"password" validate:"required,min=6,max=500"`
 	CreatedAt time.Time       `json:"createdAt"`
 	UpdatedAt time.Time       `json:"updatedAt"`
 	DeletedAt *gorm.DeletedAt `json:"deletedAt" gorm:"index"`
