83940c514553abe412125fa55155e0be1156102f
Luis Portillo <devcorvus.contact@gmail.com>
2022-10-06 21:41:23 -0400

    feat: validate_struct in utils

---
+++ utils/validate_struct.go
@@ -0,0 +1,26 @@
+package utils
+
+import "github.com/go-playground/validator/v10"
+
+type errorResponse struct {
+	Field    string
+	Required string
+	Value    string
+}
+
+var validate = validator.New()
+
+func ValidateStruct[T any](s T) []*errorResponse {
+	var errors []*errorResponse
+	err := validate.Struct(s)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var element errorResponse
+			element.Field = err.StructNamespace()
+			element.Required = err.Tag()
+			element.Value = err.Param()
+			errors = append(errors, &element)
+		}
+	}
+	return errors
+}
