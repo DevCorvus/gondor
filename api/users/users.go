12a3d7f4d520af069eacc436b4783a04105c29cb
Luis Portillo <devcorvus.contact@gmail.com>
2022-10-07 22:35:42 -0400

    feat: delete user handler

--- api/users/users.go
+++ api/users/users.go
@@ -3,6 +3,7 @@ package users
 import (
 	"github.com/DevCorvus/gondor/database"
 	"github.com/DevCorvus/gondor/database/models"
+	"github.com/DevCorvus/gondor/middlewares"
 	"github.com/DevCorvus/gondor/utils"
 
 	"github.com/gofiber/fiber/v2"
@@ -14,6 +15,7 @@ func RegisterHandlers(r fiber.Router) {
 	router := r.Group("/users")
 
 	router.Post("/", create)
+	router.Delete("/", middlewares.UserIsAuthenticated, delete)
 }
 
 func create(c *fiber.Ctx) error {
@@ -32,7 +34,20 @@ func create(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusConflict)
 	}
 
+	// TODO: Hash password before
 	db.Create(&user)
 
 	return c.SendStatus(fiber.StatusCreated)
+}
+
+func delete(c *fiber.Ctx) error {
+	userId := c.Cookies("session")
+
+	if result := db.Delete(&models.User{}, userId); result.Error != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	c.ClearCookie("session")
+
+	return c.SendStatus(fiber.StatusNoContent)
 }
